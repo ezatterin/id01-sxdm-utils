b91811a9831182727e8eb530d4264f57d0d2e573
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2021-09-15 16:17:59 +0200

    removed test from setup.py to allow installation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='E. Zatterin',
     author_email='edoardo.zatterin@esrf.fr',
-    packages=['sxdm', 'sxdm.test'],
+    packages=['sxdm'],
     license='LICENSE.txt',
     description='Code to treat Scanning X-ray Diffraction Microscopy data\
          collected on beamline ID01 at ESRF',
