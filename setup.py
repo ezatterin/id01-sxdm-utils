0164a65716c0855e9fd3f4cf758dc2fb3c4e3211
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2022-01-18 15:32:42 +0100

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,14 @@ setup(
     long_description=open("README.md").read(),
     install_requires=[
         "numpy >= 1.1.1",
-        "matplotlib",
-        "ipywigdets",
+        "matplotlib==3.4.3",
+        "ipywidgets",
         "tqdm",
-        "silx" # xsocs, id01lib
+        "silx",
+        "wheel",
+        "pandas",
+        "xrayutilities",
+        "ipympl==0.7.0" # xsocs, id01lib
     ],
 )
 
