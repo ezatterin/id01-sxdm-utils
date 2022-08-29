f6d573af62200b6a1f7195da30aac22a86c9d7fa
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2022-08-29 18:44:49 +0200

    comestic

--- tests/xsocs_shift.py
+++ tests/xsocs_shift.py
@@ -4,13 +4,16 @@ import shutil
 import numpy as np
 import time
 
-path_dset = "/data/id01/inhouse/edo/tutorial_data/ech_1_night1/ech_1_night1_0010/ech_1_night1_0010.h5"
+path_dset = (
+    "/data/id01/inhouse/edo/tutorial_data/ech_1_night1/ech_1_night1_0010/"
+    "ech_1_night1_0010.h5"
+)
 path_out = "test-data"
 
 shifts = np.array([[0.0, 0.0], [4.0, 0.0], [9.0, -1.0], [14.0, -1.0], [17.0, -3.0]])
 
 if path_out in os.listdir("."):
-    shutil.rmtree(path_out) # remove non-empty dir
+    shutil.rmtree(path_out)
 
 os.mkdir(path_out)
 
