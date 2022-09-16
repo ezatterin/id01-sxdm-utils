0d2e4e911a54c2c9f4e492d1019c91b980c9e8ad
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2022-09-16 17:18:28 +0200

    formatting

--- sxdm/utils/bliss.py
+++ sxdm/utils/bliss.py
@@ -132,9 +132,7 @@ def parse_scan_command(command):
     return cmd_dict
 
 
-def make_xsocs_links(
-    path_dset, path_out, scan_nums, detector=None, name_outh5=None
-):
+def make_xsocs_links(path_dset, path_out, scan_nums, detector=None, name_outh5=None):
     """
     Generates a set of .h5 files to be fed to XSOCS from a 3D-SXDM dataset.
     The files contain *links* to the original data, not the data itself.
@@ -170,35 +168,35 @@ def make_xsocs_links(
 
     # open the dataset file
     with h5py.File(path_dset, "r") as h5f:
-        _name_dset = os.path.basename(path_dset).split(".")[0]
+        name_dset = os.path.basename(path_dset).split(".")[0]
 
         # using all scan numbers in file?
         if scan_nums is None:
-            print(f"> Using all scan numbers in {_name_dset}")
-            _scan_idxs = range(1, len(list(h5f.keys())) + 1)
-            _commands = [h5f[f"{s}.1/title"][()].decode() for s in _scan_idxs]
-            _scan_nums = [
+            print(f"> Using all scan numbers in {name_dset}")
+            scan_idxs = range(1, len(list(h5f.keys())) + 1)
+            commands = [h5f[f"{s}.1/title"][()].decode() for s in scan_idxs]
+            scan_nums = [
                 f"{s}.1"
-                for s, c in zip(_scan_idxs, _commands)
+                for s, c in zip(scan_idxs, commands)
                 if any([s in c for s in ("sxdm", "kmap")])
             ]
         else:
             try:
-                _scan_nums = [f"{int(x)}.1" for x in scan_nums]
+                scan_nums = [f"{int(x)}.1" for x in scan_nums]
             except ValueError:  # not a list of int
-                _scan_nums = scan_nums
+                scan_nums = scan_nums
             print(
-                f"> Selecting scans {_scan_nums[0]} --> {_scan_nums[-1]} in {_name_dset}"
+                f"> Selecting scans {scan_nums[0]} --> {scan_nums[-1]} in {name_dset}"
             )
-            _commands = [h5f[f"{s}/title"][()].decode() for s in _scan_nums]
+            commands = [h5f[f"{s}/title"][()].decode() for s in scan_nums]
 
         # name the output files
         if name_outh5 is None:
-            name_outh5 = _name_dset
+            name_outh5 = name_dset
 
         # detector?
         if detector is None:
-            detector = get_detector_aliases(path_dset, _scan_nums[0])
+            detector = get_detector_aliases(path_dset, scan_nums[0])
             if len(detector) > 1:
                 msg = f"Found multiple detector groups: {detector}, select"
                 msg += "one by explicitly setting the `det` keyword argument"
@@ -207,7 +205,7 @@ def make_xsocs_links(
                 detector = detector[0]
         else:
             detector = detector
-        print(f'> Selecting detector {detector}')
+        print(f"> Selecting detector {detector}")
 
         # generate output master file
         out_h5f_master = f"{path_out}/{name_outh5}_master.h5"
@@ -215,25 +213,25 @@ def make_xsocs_links(
             pass  # overwrite master file
 
         # load counters, positioners, and other params for each scan
-        for scan_num, command in zip(_scan_nums, _commands):
+        for scan_num, command in zip(scan_nums, commands):
 
-            _entry = h5f[scan_num]
-            _instr = _entry["instrument/"]
+            entry = h5f[scan_num]
+            instr = entry["instrument/"]
 
             # get some metadata
-            start_time = _entry["start_time"][()].decode()
-            direct_beam = [_instr[f"{detector}/beam_center_{x}"] for x in ("y", "x")]
-            det_distance = _instr[f"{detector}/distance"]
+            start_time = entry["start_time"][()].decode()
+            direct_beam = [instr[f"{detector}/beam_center_{x}"] for x in ("y", "x")]
+            det_distance = instr[f"{detector}/distance"]
 
-            _pixsizes = [_instr[f"{detector}/{m}_pixel_size"][()] for m in ("y", "x")]
+            pix_sizes = [instr[f"{detector}/{m}_pixel_size"][()] for m in ("y", "x")]
             chan_per_deg = [
-                np.tan(np.radians(1)) * det_distance / pxs for pxs in _pixsizes
+                np.tan(np.radians(1)) * det_distance / pxs for pxs in pix_sizes
             ]
-            energy = xu.lam2en(_instr["monochromator/WaveLength"][()] * 1e10)
+            energy = xu.lam2en(instr["monochromator/WaveLength"][()] * 1e10)
 
             # get counters
             counters = [
-                x for x in _instr if _instr[x].attrs.get("NX_class") == "NXdetector"
+                x for x in instr if instr[x].attrs.get("NX_class") == "NXdetector"
             ]
 
             # Why am I removing this? I forgot.
@@ -246,15 +244,15 @@ def make_xsocs_links(
 
             # get piezo coordinates
             pi_positioners = [
-                x for x in _instr if _instr[x].attrs.get("NX_class") == "NXpositioner"
+                x for x in instr if instr[x].attrs.get("NX_class") == "NXpositioner"
             ]
-            positioners = [x for x in _instr["positioners"]]
+            positioners = [x for x in instr["positioners"]]
 
             # more parameters
-            _entry_name = scan_num  # <-- ends up in output h5 fname
-            _command_params = parse_scan_command(command)
+            entry_name = scan_num  # <-- ends up in output h5 fname
+            command_params = parse_scan_command(command)
 
-            out_h5f = f"{path_out}/{name_outh5}_{_entry_name}.h5"
+            out_h5f = f"{path_out}/{name_outh5}_{entry_name}.h5"
 
             # write links to individual XSOCS-compatible files
             with XsocsH5.XsocsH5Writer(out_h5f, "w") as xsocsh5f:  # overwrite
@@ -263,46 +261,46 @@ def make_xsocs_links(
                 XsocsH5Writer methods
                 --> make links to scan parameters
                 """
-                xsocsh5f.create_entry(_entry_name)  # creates NX skeleton
+                xsocsh5f.create_entry(entry_name)  # creates NX skeleton
                 xsocsh5f.set_scan_params(
-                    _entry_name, **_command_params
+                    entry_name, **command_params
                 )  # "scan" folder contents
 
-                xsocsh5f.set_beam_energy(energy, _entry_name)
-                xsocsh5f.set_chan_per_deg(chan_per_deg, _entry_name)
-                xsocsh5f.set_direct_beam(direct_beam, _entry_name)
-                xsocsh5f.set_image_roi_offset([0, 0], _entry_name)  # hardcoded for now
+                xsocsh5f.set_beam_energy(energy, entry_name)
+                xsocsh5f.set_chan_per_deg(chan_per_deg, entry_name)
+                xsocsh5f.set_direct_beam(direct_beam, entry_name)
+                xsocsh5f.set_image_roi_offset([0, 0], entry_name)  # hardcoded for now
 
                 """
                 XsocsH5Base methods
                 --> make links to data and counters
                 """
-                xsocsh5f._set_scalar_data(f"{_entry_name}/title", command)
-                xsocsh5f._set_scalar_data(f"{_entry_name}/start_time", start_time)
+                xsocsh5f._set_scalar_data(f"{entry_name}/title", command)
+                xsocsh5f._set_scalar_data(f"{entry_name}/start_time", start_time)
 
                 for c in counters:
                     if c == detector:
                         xsocsh5f.add_file_link(
-                            f"{_entry_name}/measurement/image/data",
+                            f"{entry_name}/measurement/image/data",
                             path_dset,
                             f"{scan_num}/measurement/{c}",
                         )
                     else:
                         xsocsh5f.add_file_link(
-                            f"{_entry_name}/measurement/{c}",
+                            f"{entry_name}/measurement/{c}",
                             path_dset,
                             f"{scan_num}/measurement/{c}",
                         )
                 for p in positioners:
                     if p == "delta":
                         xsocsh5f.add_file_link(
-                            f"{_entry_name}/instrument/positioners/del",
+                            f"{entry_name}/instrument/positioners/del",
                             path_dset,
                             f"{scan_num}/instrument/positioners/{p}",
                         )
                     else:
                         xsocsh5f.add_file_link(
-                            f"{_entry_name}/instrument/positioners/{p}",
+                            f"{entry_name}/instrument/positioners/{p}",
                             path_dset,
                             f"{scan_num}/instrument/positioners/{p}",
                         )
@@ -311,25 +309,25 @@ def make_xsocs_links(
                     try:
                         new_c = pi_motor_names[pp]
                         xsocsh5f.add_file_link(
-                            f"{_entry_name}/measurement/{new_c}",
+                            f"{entry_name}/measurement/{new_c}",
                             path_dset,
                             f"{scan_num}/instrument/{pp}/value",
                         )
                     except KeyError:
                         pass
 
-                _imgnr = np.arange(_entry[f"measurement/{detector}"].shape[0])
-                xsocsh5f._set_array_data(f"{_entry_name}/measurement/imgnr", _imgnr)
+                _imgnr = np.arange(entry[f"measurement/{detector}"].shape[0])
+                xsocsh5f._set_array_data(f"{entry_name}/measurement/imgnr", _imgnr)
 
                 xsocsh5f.add_file_link(
-                    f"{_entry_name}/technique", path_dset, f"{scan_num}/technique"
+                    f"{entry_name}/technique", path_dset, f"{scan_num}/technique"
                 )
 
             # write links to XSOCS master file
             with XsocsH5.XsocsH5MasterWriter(out_h5f_master, "a") as master:
-                master.add_entry_file(_entry_name, os.path.basename(out_h5f))
+                master.add_entry_file(entry_name, os.path.basename(out_h5f))
 
             # print
-            print(f"\r> Linking # {scan_num}/{_scan_nums[-1]}", flush=True, end=" ")
+            print(f"\r> Linking # {scan_num}/{scan_nums[-1]}", flush=True, end=" ")
 
         print("\n> Done!\n")
