cf73e69bca9ce5dd4ae1ee40a4432d858a85d77e
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2024-02-01 16:51:17 +0100

    better naming

--- sxdm/widgets/bliss/__init__.py
+++ sxdm/widgets/bliss/__init__.py
@@ -1,3 +1,3 @@
-from .InspectROI import InspectROI
+from .InspectSXDMCounter import InspectROI, InspectSXDMCounter
 from .GetShift import GetShift, GetShiftCustom
 from .Inspect4DSXDM import Inspect4DSXDM
