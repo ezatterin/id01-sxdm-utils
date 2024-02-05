0f7e757279bab082b2a1d75c76701538e6fdb715
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2024-02-05 12:01:56 +0100

    check if output folder exists

--- tests/xsocs_shift.py
+++ tests/xsocs_shift.py
@@ -9,7 +9,8 @@ path_out = "examples/data/MA1234/id01/20230710/sample_analysis/"
 
 shifts = np.array([[0.0, 0.0], [4.0, 0.0], [9.0, -1.0], [14.0, -1.0], [17.0, -3.0]])
 
-os.mkdir(path_out)
+if not os.path.isdir(path_out):
+    os.mkdir(path_out)
 
 sxdm.utils.bliss.make_xsocs_links(path_dset, path_out, None)
 path_master = f"{path_out}/sample_0001_master.h5"
