a9133e13d25a2a3e3de8afba5331b60841bf4c97
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2024-02-05 13:06:53 +0100

    fix path

--- tests/xsocs_qconvert.py
+++ tests/xsocs_qconvert.py
@@ -3,7 +3,7 @@ import time
 
 from xsocs.process.qspace.helpers import kmap_2_qspace
 
-path_out = "examples/data/MA1234/id01/20230710/sample_analysis"
+path_out = "../examples/data/MA1234/id01/20230710/sample_analysis"
 path_master = f"{path_out}/sample_0001_master_shifted.h5"  
 path_qspace = f"{path_out}/qspace_shift.h5"
 
@@ -18,7 +18,7 @@ try:
     )
     t1 = time.time()
 
-except (TypeError, ValueError):
+except (TypeError, ValueError): # offsets not defined
     print("Using xsocs-upstream\n")
     t0 = time.time()
     kmap_2_qspace(path_master, path_qspace, (10, 10, 10), overwrite=True)
--- tests/xsocs_shift.py
+++ tests/xsocs_shift.py
@@ -4,8 +4,8 @@ import numpy as np
 import time
 
 # weirdly it seems the GL pipeline sees the root dir
-path_dset = "examples/data/MA1234/id01/20230710/sample/sample_0001/sample_0001.h5"
-path_out = "examples/data/MA1234/id01/20230710/sample_analysis/"
+path_dset = "../examples/data/MA1234/id01/20230710/sample/sample_0001/sample_0001.h5"
+path_out = "../examples/data/MA1234/id01/20230710/sample_analysis/"
 
 shifts = np.array([[0.0, 0.0], [4.0, 0.0], [9.0, -1.0], [14.0, -1.0], [17.0, -3.0]])
 
@@ -16,11 +16,11 @@ sxdm.utils.bliss.make_xsocs_links(path_dset, path_out, None)
 path_master = f"{path_out}/sample_0001_master.h5"
 
 t0 = time.time()
-sxdm.process.xsocs.shift_xsocs_data(path_master, path_out, shifts)
+sxdm.process.xsocs.shift_xsocs_data(path_master, path_out, shifts, overwrite=True)
 t1 = time.time()
 
 print(f"\n\n >> Shifting took {t1-t0:.1f}s")
 
 ls = os.listdir(path_out)
-print('Output directory contents:')
-_ = [print(f'\t > {l}') for l in ls]
+print("Output directory contents:")
+_ = [print(f"\t > {l}") for l in ls]
