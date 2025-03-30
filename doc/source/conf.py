2837a4d94c65d6ad12b7007cc2e4735cb0c3e0eb
Edoardo Zatterin <ezatterin@yahoo.it>
2025-03-30 14:28:57 +0200

    added gl bottom icon + nbs runs on win11 - build_docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -79,6 +79,26 @@ exclude_patterns = ["_tests.md", "build"]
 html_theme = "furo"
 html_title = "ID01-SXDM Documentation"
 
+html_theme_options = {
+    "source_repository": "https://gitlab.esrf.fr/id01-science/id01-sxdm-utils",
+    "source_branch": "main",
+    "source_directory": "doc/",
+    "footer_icons": [
+        {
+            "name": "GitLab",
+            "url": "https://gitlab.esrf.fr/id01-science/id01-sxdm-utils",
+            "html": "",
+            "class": "fa-brands fa-solid fa-gitlab fa-2x",
+        },
+    ],
+}
+
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
