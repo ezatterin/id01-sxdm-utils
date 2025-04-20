83792dbdbc7c8c113bdbacd40a3c0da15570e1b8
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2025-04-20 22:32:27 +0200

    cleanup and added myst block attributes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,10 +6,6 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 
@@ -22,15 +18,11 @@ project = "id01-sxdm-utils"
 copyright = "2024, Edoardo Zatterin"
 author = "Edoardo Zatterin"
 
-# The full version, including alpha/beta/rc tags
 release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "myst_nb",
     "myst_parser",
@@ -53,7 +45,10 @@ myst_enable_extensions = [
     "deflist",
     "dollarmath",
     "html_image",
+    "attrs_block",
 ]
+
+myst_heading_anchors = 3
 
 myst_url_schemes = ("http", "https", "mailto")
 
@@ -66,12 +61,8 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_tests.md", "build"]
 
 
