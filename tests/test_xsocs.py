c36e9b040993d0b555814f267164acbcbf6cebe3
Edoardo Zatterin (rnice9) <edoardo.zatterin@esrf.fr>
2025-05-23 11:57:51 +0200

    hotfix data paths | run_tests build_docs

--- tests/test_xsocs.py
+++ tests/test_xsocs.py
@@ -5,17 +5,19 @@ import numpy as np
 
 def test_xsocs_shift():
     # weirdly it seems the GL pipeline sees the root dir
-    path_dset = "doc/source/examples/data/MA1234/id01/20230710/sample/sample_0001/sample_0001.h5"
-    path_out = "doc/source/examples/data/MA1234/id01/20230710/sample_analysis/"
+    path_dset = "/doc/source/examples/data/MA1234/id01/20230710/RAW_DATA/InGaN/InGaN_0001/InGaN_0001.h5"
+    path_out = (
+        "/doc/source/examples/data/MA1234/id01/20230710/PROCESSED_DATA/InGaN_processed/"
+    )
 
     shifts = np.array([[0.0, 0.0], [4.0, 0.0], [9.0, -1.0], [14.0, -1.0], [17.0, -3.0]])
 
     if not os.path.isdir(path_out):
-        os.mkdir(path_out)
+        os.makedirs(path_out)
 
     sxdm.utils.bliss.make_xsocs_links(path_dset, path_out, None)
 
-    path_master = f"{path_out}/sample_0001_master.h5"
+    path_master = f"{path_out}/InGaN_0001_master.h5"
     ret = sxdm.process.xsocs.shift_xsocs_data(
         path_master, path_out, shifts, overwrite=True
     )
@@ -23,9 +25,11 @@ def test_xsocs_shift():
 
 
 def test_xsocs_qconv():
-    path_out = "doc/source/examples/data/MA1234/id01/20230710/sample_analysis"
-    path_master = f"{path_out}/sample_0001_master_shifted.h5"
-    path_qspace = f"{path_out}/qspace_shift.h5"
+    path_out = (
+        "/doc/source/examples/data/MA1234/id01/20230710/PROCESSED_DATA/InGaN_processed/"
+    )
+    path_master = f"{path_out}/InGaN_0001_master_shifted.h5"
+    path_qspace = f"{path_out}/InGaN_qspace_shift.h5"
 
     offsets = {"eta": 0, "delta": 0, "phi": 0, "roby": 1, "nu": 0.5}
 
